fe0afa4847d73704cd1053b9b35ee0103d01732f
J.R. Lorence <jrlorence@users.noreply.github.com>
2016-12-11 09:42:04 -0500

    Re-orged to LED control based methodology

--- temp_button.py
+++ temp_button.py
@@ -1,35 +1,70 @@
 #!/usr/bin/python
+###############################################
+# Author: J.R. Lorence <jrlorence@gmail.com>
+# Date:   12/11/2016
+###############################################
 
+# Required python modules
 import RPi.GPIO as GPIO
 from time import sleep
 
-GPIO.setmode(GPIO.BCM)
-
+# Global static variables
+ON = 1
+OFF = 0
 in_pin = 19
-out_pin = 18
+LED_PIN = 18
 
-GPIO.setup(in_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(out_pin,GPIO.OUT)
-GPIO.output(out_pin,0)
 
-try:
-	last_state = 0
-	while True:
-		# save time, output gets the input
-		button_pressed = GPIO.input(in_pin)
-		if button_pressed:
-			GPIO.output(out_pin,GPIO.HIGH)
-		else:
-			GPIO.output(out_pin,GPIO.LOW)
+def main():
+	# Setup board inputs, outputs, pins, etc
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(in_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+	GPIO.setup(LED_PIN,GPIO.OUT)
+	GPIO.output(LED_PIN,0)
 
-		# smooth button debounce and long presses
-		if button_pressed and not last_state:
-			print "State switched"
-			# deal with button debounch
-			sleep(0.3)
+	# Wait for user button press input
+	try:
+		last_state = 0
+		while True:
+			button_pressed = GPIO.input(in_pin)
+	
+			# smooth button debounce and long presses
+			if button_pressed and not last_state:
+				print "Button pressed"
+				do_something()
+				# deal with button debounce
+				sleep(0.3)
+	
+			last_state = button_pressed
 
-		last_state = button_pressed
-		#sleep(0.1)
+	finally:
+		GPIO.cleanup()
 
-finally:
-	GPIO.cleanup()
+def do_something():
+	# Flash LED to ack user input
+	flash_led()
+
+	# Do the meat of the work
+	toggle_led(ON)
+	sleep(2)
+
+	# Flash LED to ack input is complete
+	flash_led()
+	toggle_led(OFF)
+	print "Did something"
+
+def toggle_led(state):
+	GPIO.output(LED_PIN,state)
+
+def flash_led(num_flashes=2,delay=0.07):
+	toggle_led(OFF)
+	while (num_flashes > 0):
+		toggle_led(ON)
+		sleep(delay)
+		toggle_led(OFF)
+		sleep(delay)
+		num_flashes -= 1
+	toggle_led(OFF)
+
+if __name__ == "__main__":
+	main()
